a0eb75a7491916a9aa1218500740aff6393729c4
ijooswk <ijooswk@gmail.com>
2014-04-24 21:02:18 +0900

    사진을 찍으면 노티피케이션으로 연동되는 소스 추가 구현
    나중에 노티피케이션에서 DB에 직접 쑤셔넣을 수 있는 코드를 구현할 예정

--- gen/pe/ijooswk/pic2count/R.java
+++ gen/pe/ijooswk/pic2count/R.java
@@ -25,16 +25,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080006;
+        public static final int image=0x7f080002;
         public static final int pager=0x7f080000;
-        public static final int pic_gallery_btn=0x7f080002;
-        public static final int pic_gallery_image=0x7f080003;
+        public static final int pic_gallery_btn=0x7f080004;
+        public static final int pic_gallery_image=0x7f080005;
         public static final int section_label=0x7f080001;
+        public static final int text=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main=0x7f030001;
-        public static final int pic_detail_view=0x7f030002;
+        public static final int notification_layout=0x7f030002;
+        public static final int pic_detail_view=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/pe/ijooswk/pic2count/Utils/CameraEventReceiver.java
+++ src/pe/ijooswk/pic2count/Utils/CameraEventReceiver.java
@@ -1,19 +1,89 @@
 package pe.ijooswk.pic2count.Utils;
 
+import java.io.IOException;
+
+import pe.ijooswk.pic2count.R;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class CameraEventReceiver extends BroadcastReceiver{
-
+	
+	Context mContext;
+//	ExifInterface exif;
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Cursor cursor = context.getContentResolver().query(intent.getData(), null, null, null, null);
 	    cursor.moveToFirst();
 	    String image_path = cursor.getString(cursor.getColumnIndex("_data"));
 	    Toast.makeText(context, "New Photo is Saved as : -" + image_path, 1000).show();
+	    mContext = context;
+	    
+	    iniNotification(image_path);
 	}
 
+	public void iniNotification(String image){
+		  int icon = R.drawable.ic_launcher;
+	        long when = System.currentTimeMillis();
+	        Notification notification = new Notification(icon, "Custom Notification", when);
+
+	        NotificationManager mNotificationManager = (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+	        RemoteViews contentView = new RemoteViews(mContext.getPackageName(), R.layout.notification_layout);
+	        
+	        contentView.setImageViewBitmap(R.id.image, getScaledBitmap(image, 300, 300));
+//	        contentView.setImageViewResource(R.id.image, R.drawable.ic_launcher);
+	        contentView.setTextViewText(R.id.text, "This is a custom layout");
+	        notification.contentView = contentView;
+
+	        Intent notificationIntent = new Intent(mContext, getClass());
+	        PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, notificationIntent, 0);
+	        notification.contentIntent = contentIntent;
+
+	        notification.flags |= Notification.FLAG_NO_CLEAR; //Do not clear the notification
+	        notification.defaults |= Notification.DEFAULT_LIGHTS; // LED
+	        notification.defaults |= Notification.DEFAULT_VIBRATE; //Vibration
+	        notification.defaults |= Notification.DEFAULT_SOUND; // Sound
+
+	        mNotificationManager.notify(1, notification);
+	}
+	
+	private Bitmap getScaledBitmap(String picturePath, int width, int height) {
+	    BitmapFactory.Options sizeOptions = new BitmapFactory.Options();
+	    sizeOptions.inJustDecodeBounds = true;
+	    BitmapFactory.decodeFile(picturePath, sizeOptions);
+
+	    int inSampleSize = calculateInSampleSize(sizeOptions, width, height);
+
+	    sizeOptions.inJustDecodeBounds = false;
+	    sizeOptions.inSampleSize = inSampleSize;
+
+	    return BitmapFactory.decodeFile(picturePath, sizeOptions);
+	}
+	
+	private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+	    // Raw height and width of image
+	    final int height = options.outHeight;
+	    final int width = options.outWidth;
+	    int inSampleSize = 1;
+
+	    if (height > reqHeight || width > reqWidth) {
+	        final int heightRatio = Math.round((float) height / (float) reqHeight);
+	        final int widthRatio = Math.round((float) width / (float) reqWidth);
+	        inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+	    }
+
+	    return inSampleSize;
+	}
 }
